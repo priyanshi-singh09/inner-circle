1763ab2cd64162929f40eb34ab175cd91f3edf33
priyanshi-singh09 <ps3109809@gmail.com>
2026-09-05 13:13:05 +0530

    Add Spring Boot application entry point

---
+++ backend/src/main/java/com/innercircle/InnerCircleApplication.java
@@ -0,0 +1,12 @@
+package com.innercircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerCircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerCircleApplication.class, args);
+    }
+}
