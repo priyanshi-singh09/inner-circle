a8f98aa40ec5d0d375688f311e2b5eb86afa2fb5
priyanshi-singh09 <ps3109809@gmail.com>
2026-09-05 13:42:36 +0530

    fix: remove incorrect block repository

--- backend/src/main/java/com/innercircle/repository/BlockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.innercircle.repository;
-
-import com.innercircle.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface BlockRepository extends JpaRepository<User, UUID> {
-}
