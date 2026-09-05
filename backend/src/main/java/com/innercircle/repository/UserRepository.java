89e57b704d8c99a4fa085c199f9fc276a6863747
priyanshi-singh09 <ps3109809@gmail.com>
2026-09-05 13:13:55 +0530

    Add User repository

---
+++ backend/src/main/java/com/innercircle/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.innercircle.repository;
+
+import com.innercircle.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmail(String email);
+    boolean existsByHandle(String handle);
+    Optional<User> findByEmail(String email);
+}
