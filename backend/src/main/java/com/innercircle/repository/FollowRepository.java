8d32c22d2d2461602026dc315f748cf107893e49
priyanshi-singh09 <ps3109809@gmail.com>
2026-09-05 14:06:05 +0530

    fix: make follow repository compile with embedded id

--- backend/src/main/java/com/innercircle/repository/FollowRepository.java
+++ backend/src/main/java/com/innercircle/repository/FollowRepository.java
@@ -1,7 +1,6 @@
 package com.innercircle.repository;
 
 import com.innercircle.entity.Follow;
-import com.innercircle.entity.FollowId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.UUID;
 
-public interface FollowRepository extends JpaRepository<Follow, FollowId> {
+public interface FollowRepository extends JpaRepository<Follow, Object> {
     boolean existsByFollower_IdAndFollowing_Id(UUID followerId, UUID followingId);
     void deleteByFollower_IdAndFollowing_Id(UUID followerId, UUID followingId);
     @Query("""
